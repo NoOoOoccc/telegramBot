9cb611ed022aad92dacc0f3476f8f050ad102bf6
夜行丨神鱼 <a68116780@qq.com>
2023-07-23 19:03:54 +0800

    修改查找规则  增加检索比对歌手名 是否完全吻合

--- cloudMusic.py
+++ cloudMusic.py
@@ -7,12 +7,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 # 根据搜索结果 获取歌曲id
-def getMusicId(searchName):
+def getMusicId(searchName, songName, singer):
     option = webdriver.ChromeOptions()
-    #伪造请求头
+    # 伪造请求头
     option.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/{}"
                         " (XHTML, like Gecko) Chrome/115.0.5790.98 Safari/{}")
-    #隐藏浏览器窗口
+    # 隐藏浏览器窗口
     option.add_argument('headless')
     # 去除图片及css样式
     prefs = {"profile.managed_default_content_settings.images": 2, 'permissions.default.stylesheet': 2}
@@ -23,12 +23,32 @@ def getMusicId(searchName):
     try:
         url = 'https://music.163.com/#/search/m/?s=' + searchName
         driver.get(url)
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 1)
         # 切换到name =  contentFrame  的iframe中
         wait.until(EC.frame_to_be_available_and_switch_to_it((By.NAME, "contentFrame")))
         soup = BeautifulSoup(driver.page_source, "html.parser")
         # 获取第一个class为ply的a标签中的id
-        row = soup.find_all("a", {"class": "ply"})[0].get('id')
+        # row = soup.find_all("a", {"class": "ply"})[0].get('id')
+        div_tags = soup.find_all('div', {'class': 'td w1'})
+        # 比对歌手正确标识
+        found = False
+        # 索引标记
+        index = 0
+        for div in div_tags:
+            if found:
+                break
+            text_div = div.find('div', {'class': 'text'})
+            a_tags = text_div.find('a')
+            for a in a_tags:
+                if a.text == singer:
+                    # 修改标识以退出循环
+                    found = True
+                    break
+                else:
+                    index = index + 1
+        # 获取索引标记的id
+        row = soup.find_all("a", {"class": "ply"})[index].get('id')
+        print(row)
         id = row.split('song_')[1]
         print(id)
         lyc = getMusicLyc(id, driver)
@@ -38,13 +58,14 @@ def getMusicId(searchName):
         print('except:', e)
         return '歌曲id获取失败，请检查转发格式'
 
+
 # 根据Id获取歌词
 def getMusicLyc(musicId, driver):
     print('开始获取歌词')
     lyc_url = "https://music.163.com/#/song?id=" + musicId  # 根据歌曲的 ID 号拼接出下载的链接。歌曲直链获取的方法参考文前的注释部分。
     try:
         driver.get(lyc_url)
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 1)
         # 切换到name =  contentFrame  的iframe中
         wait.until(EC.frame_to_be_available_and_switch_to_it((By.NAME, "contentFrame")))
         # 获取网页元素
@@ -67,4 +88,4 @@ def getMusicLyc(musicId, driver):
 
 # 测试使用
 if __name__ == '__main__':
-    MusicId = getMusicId('修炼爱情')
+    MusicId = getMusicId('夜曲 周杰伦', '夜曲', '周杰伦')
