345d3f02ecf860604fbeaf646cc3d68d619cc08c
夜行丨神鱼 <a68116780@qq.com>
2023-07-29 16:00:52 +0800

    增加了功能  使私聊机器人的mp4文件  转为gif图片

--- bot.py
+++ bot.py
@@ -1,18 +1,40 @@
 # 导入需要的库
 import logging
-import threading
+
 # 导入网易云音乐爬取脚本
 import cloudMusic
+
 # 导入图片处理
 import pictureHandle
+
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, Filters, MessageHandler
-from telegram.ext import ConversationHandler
+from telegram.ext import Updater, CommandHandler, Filters, CallbackContext, MessageHandler
 
 # 消息回复超时时间
 MessageTimeOut = 600
 # 异步线程数量
 threadWorkers = 8
+
+
+def mp4ToGif(update: Update, context: CallbackContext) -> None:
+    print("接收到视频,进入了mp4ToGif")
+    if update.message.document:
+        # 获取文件名
+        file_name = update.message.document.file_name
+        # 获取文件后缀
+        file_extension = file_name.split('.')[-1]
+        print("文件后缀是:", file_extension)
+        if file_extension == "mp4":
+            # 获取文件的唯一标识符
+            file_id = update.message.document.file_id
+            # 使用bot的get_file方法获取文件
+            video_file = context.bot.get_file(file_id)
+            # 获取文件的字节流
+            video_bytes = video_file.download_as_bytearray()
+            # 调用videoToGIF函数进行转换
+            gif_bytes = pictureHandle.videoToGIF(video_bytes)
+            # 发送转换后的GIF文件
+            context.bot.send_document(update.message.chat_id, gif_bytes)
 
 
 def photo(update: Update, context: CallbackContext):
@@ -26,12 +48,23 @@ def photo(update: Update, context: CallbackContext):
         file_id = sticker.file_id
         # 使用bot的get_file方法获取文件
         photo_file = context.bot.get_file(file_id)
+        # 获取文件的后缀格式
+        file_extension = photo_file.file_path.split('.')[-1]
+        print(file_extension)
         # 获取文件的字节流
         photo_bytes = photo_file.download_as_bytearray()
-        # 转换为jpeg格式
-        byte_arr = pictureHandle.pictureToJPEG(photo_bytes)
-        # 回复图片
-        context.bot.send_photo(update.message.chat_id, byte_arr)
+        if file_extension == "webm":
+            print("这是一个动态贴纸，格式是.webm")
+            # 转换为gif格式
+            byte_arr = pictureHandle.pictureToGIF(photo_bytes)
+            # 回复图片
+            context.bot.send_animation(update.message.chat_id, byte_arr)
+        elif file_extension == "webp":
+            print("这是一个静态贴纸，格式是.webp")
+            # 转换为png格式
+            byte_arr = pictureHandle.pictureToPNG(photo_bytes)
+            # 回复图片
+            context.bot.send_photo(update.message.chat_id, byte_arr)
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -49,6 +82,7 @@ def word_of_song(update: Update, context: CallbackContext) -> None:
     # 如果消息包含音频 根据消息标题获取歌名  到网易云进行搜索爬取歌词
     if update.message.audio is not None:
         try:
+            print(update.message.audio)
             # 获取歌名
             songName = update.message.audio.title.title()
             # 获取歌手
@@ -63,13 +97,15 @@ def word_of_song(update: Update, context: CallbackContext) -> None:
 
 
 # 创建Updater实例，需要提供有效的Telegram bot API token
-updater = Updater('6062648071:AAFlgKl1aHftCPy3nkwQ4cIuEZSm0ufHJAM', use_context=True, workers=threadWorkers)
+updater = Updater('6062648071:AAFlgKl1aHftCPy3nkwQ4cIuEZSm0ufHJAM', use_context=True,workers=threadWorkers)
 
 # 将/start和/test命令的处理函数添加到dispatcher
-updater.dispatcher.add_handler(MessageHandler(Filters.sticker, photo,run_async=True))
+updater.dispatcher.add_handler(MessageHandler(Filters.document, mp4ToGif))
+updater.dispatcher.add_handler(MessageHandler(Filters.sticker & Filters.private, photo))
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('test', test))
 updater.dispatcher.add_handler(MessageHandler(Filters.chat_type.groups, word_of_song, run_async=True))
+
 # 开始监听新的更新
 updater.start_polling()
 
